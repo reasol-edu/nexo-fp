49a41bce4b4a3e1d5ac6509ebd907a85335918c9
Luis Ramón López <lrlopez@gmail.com>
2026-05-31 19:07:32 +0200

    feat(model): AcademicYear pertenece a EducationalCentre

--- src/Entity/AcademicYear.php
+++ src/Entity/AcademicYear.php
@@ -6,14 +6,19 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Uid\Uuid;
 
 #[ORM\Entity(repositoryClass: AcademicYearRepository::class)]
+#[ORM\UniqueConstraint(name: 'uq_academic_year_centre', columns: ['name', 'educational_centre_id'])]
 class AcademicYear
 {
     #[ORM\Id]
     #[ORM\Column(type: 'uuid')]
     private readonly Uuid $id;
 
-    #[ORM\Column(length: 50, unique: true)]
+    #[ORM\Column(length: 50)]
     private ?string $name = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?EducationalCentre $educationalCentre = null;
 
     public function __construct()
     {
@@ -33,6 +38,18 @@ class AcademicYear
     public function setName(string $name): static
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEducationalCentre(): ?EducationalCentre
+    {
+        return $this->educationalCentre;
+    }
+
+    public function setEducationalCentre(?EducationalCentre $educationalCentre): static
+    {
+        $this->educationalCentre = $educationalCentre;
 
         return $this;
     }
