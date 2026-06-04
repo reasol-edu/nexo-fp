17d642c8c13d3a3099cba506e375dcc2e45617ea
Luis Ramón López <lrlopez@gmail.com>
2026-06-04 21:36:07 +0200

    chore: Eliminado estado de puesto formativo no utilizado

--- src/Entity/TrainingPositionState.php
+++ src/Entity/TrainingPositionState.php
@@ -4,7 +4,6 @@ namespace App\Entity;
 enum TrainingPositionState: string
 {
     case DRAFT = 'DRAFT';
-    case REGISTERED = 'REGISTERED';
     case PENDING = 'PENDING';
     case DONE = 'DONE';
 }
